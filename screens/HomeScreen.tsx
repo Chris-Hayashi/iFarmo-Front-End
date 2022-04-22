e9c8bfd3bad495591694fb446d0ec46d6cc82855
chris-hayashi <Chrishayashi@rocketmail.com>
2022-04-22 14:00:44 -0700

    Sort functionality working

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,6 +1,5 @@
 import { StyleSheet } from 'react-native';
 
-import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { RootStackScreenProps } from '../types';
 
--- screens/ProduceScreen.tsx
+++ screens/ProduceScreen.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { StyleSheet, FlatList, Dimensions, TouchableOpacity, ActivityIndicator, Image } from 'react-native';
-import { Card, FAB, Input, Overlay, Button, Text, Icon } from 'react-native-elements';
+import { FAB, Icon } from 'react-native-elements';
+import { IconButton, Menu, Divider, Provider } from 'react-native-paper';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Dropdown } from 'react-native-element-dropdown';
 import SearchBar from 'react-native-platform-searchbar';
@@ -17,6 +18,7 @@ export default function HomeScreen({ navigation }: RootStackScreenProps<'Home'>)
   const [itemArray, setItemArray] = useState([]);
   const [fabVisible, setFabVisible] = useState(true);
   const [overlayVisible, setOverlayVisible] = useState(false);
+  const [filterVisible, setFilterVisible] = useState(false);
   const [render, setRender] = useState(false);
 
   type OverlayComponentProps = {};
@@ -26,7 +28,7 @@ export default function HomeScreen({ navigation }: RootStackScreenProps<'Home'>)
     getProducts();
     console.log("useEffect() executed");
 
-  }, [render, search]);
+  }, [render, search, filter]);
 
 
   /* search and filter function */
@@ -45,7 +47,7 @@ export default function HomeScreen({ navigation }: RootStackScreenProps<'Home'>)
       });
   }
 
-  const postProduct = async (productObj : {}) => {
+  const postProduct = async (productObj: {}) => {
     let token: any = await AsyncStorage.getItem("auth-token");
 
     axios.post('https://nodejs-ifarmo.herokuapp.com/api/products', productObj, {
@@ -67,39 +69,59 @@ export default function HomeScreen({ navigation }: RootStackScreenProps<'Home'>)
   }
 
   return (
-    <View style={[styles.container]}>
-      <View style={{display: 'flex', flexDirection: 'row'}}>
-        <SearchBar
-          placeholder='Search'
-          onChangeText={(val) => {
-            setSearch(val);
-            // getProducts();
-          }}
-          value={search}
-          platform='ios'
-          theme='light'
-          style={styles.searchBar}
+    <Provider>
+      <View style={[styles.container]}>
+        <View style={{ display: 'flex', flexDirection: 'row' }}>
+          <SearchBar
+            placeholder='Search'
+            onChangeText={(val) => {
+              setSearch(val);
+              // getProducts();
+            }}
+            value={search}
+            platform='ios'
+            theme='light'
+            style={styles.searchBar}
+          />
+          {/* <View style={{paddingTop: 50}}> */}
+          <Menu
+            visible={filterVisible}
+            onDismiss={() => setFilterVisible(false)}
+            style={styles.menu}
+            anchor={
+              <IconButton icon='filter' style={styles.filterIcon} onPress={() => setFilterVisible(true)} />
+            }
+          >
+            <Menu.Item
+              onPress={() => {
+                setFilter('by_date');
+                setFilterVisible(false);
+              }}
+            title="Sort by date" 
+            />
+            <Menu.Item 
+            onPress={() => {
+              setFilter('by_price');
+              setFilterVisible(false);
+            }}
+            title="Sort by price" />
+          </Menu>
+          {/* </View> */}
+        </View>
+
+        <Grid items={itemArray} />
+        <FAB
+          visible={fabVisible}
+          icon={{ name: 'add', color: 'white' }}
+          color="green"
+          onPress={toggleAddItemOverlay}
+          style={styles.fab}
         />
-        <Icon
-          name='filter'
-          type='font-awesome-5'
-          solid={true}
-          style={styles.filterIcon}
-        />
+        {/* <AddItemOverlay /> */}
+        <AddItemOverlay isVisible={overlayVisible} postItem={postProduct} onBackdropPressHandler={toggleAddItemOverlay} />
+
       </View>
-
-      <Grid items={itemArray} />
-      <FAB
-        visible={fabVisible}
-        icon={{ name: 'add', color: 'white' }}
-        color="green"
-        onPress={toggleAddItemOverlay}
-        style={styles.fab}
-      />
-      {/* <AddItemOverlay /> */}
-      <AddItemOverlay isVisible={overlayVisible} postItem={postProduct} onBackdropPressHandler={toggleAddItemOverlay}/>
-
-    </View>
+    </Provider>
   );
 };
 
@@ -119,11 +141,14 @@ const styles = StyleSheet.create({
   filterIcon: {
     flex: 1,
     justifyContent: 'center',
-    marginHorizontal: 10
+    marginHorizontal: 10,
   },
   searchBar: {
     flex: 1,
     marginLeft: 10
+  },
+  menu: {
+    top: 60
   }
 });
 
